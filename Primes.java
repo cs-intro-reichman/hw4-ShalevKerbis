3f7f9c69311499bc7e3b7f4d5ebd0eee495b5780
שלו קרביס <shalev@shalevs-MacBook-Pro.local>
2024-11-28 23:56:21 +0200

    completed question 2

--- Primes.java
+++ Primes.java
@@ -1,5 +1,34 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int input = Integer.parseInt(args[0]);
+        boolean[] isPrime = new boolean[input + 1];
+        isPrime[0] = false;
+        isPrime[1] = false;
+        int j = 2;
+        int sumOfPrime = 0;
+        System.out.println("Prime numbers up to " + input + ":");
+
+        //setting all elements with index > 1 to true
+        for (int x = 2 ; x <= input ; x++){
+            isPrime[x] = true;
+        }
+        while (j * j <= input){
+            if (isPrime[j]){
+                int i = j * j;
+                while (i <= input){
+                    isPrime[i] = false;
+                    i = i + j;
+                }
+            }
+            j++;
+        }
+
+        for (int i = 0 ; i <= input ; i++){
+            if (isPrime[i] == true){
+                System.out.println(i);
+                sumOfPrime++;
+            }
+        }
+        System.out.println("There are " + sumOfPrime + " primes between 2 and " + input + " (" +  (int) (100 * (((double) sumOfPrime / input))) + "% are primes)");
     }
 }

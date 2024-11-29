da75260027d25d6d95be37bd5403636e99529962
שלו קרביס <shalev@shalevs-MacBook-Pro.local>
2024-11-29 20:06:41 +0200

    completed question 3

--- MyString.java
+++ MyString.java
@@ -18,13 +18,67 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String finalString = "";
+        for (int i = 0 ; i < str.length() ; i++){
+            if (str.charAt(i) <= 90 && str.charAt(i) >= 65){
+                finalString = finalString + (char)(str.charAt(i) + 32);
+            } else {
+                finalString = finalString + str.charAt(i);
+            }
+        }
+        return finalString;
+    }
+
+    //converting a string to an array
+    public static char[]stringToArray(String str){
+        char[] arr = new char[str.length()];
+        for (int i = 0 ; i < str.length() ; i++){
+            arr[i] = str.charAt(i);
+        }
+        return arr;
+    }
+
+    //checks if 2 char arrays are similar
+    public static boolean equalsArray(char[] arr1, char[] arr2){
+        if (arr1.length != arr2.length){
+            return false;
+        }
+        for (int i = 0 ; i < arr1.length ; i++){
+            if (arr1[i] != arr2[i]){
+                return false;
+            }
+        }
+        return true;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str1.length() < str2.length()){
+            return false;
+        }
+
+        if (str1.length() == str2.length()){
+            if (str1 == str2){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        for (int i = 0 ; i <= str1.length() - str2.length() ; i++){
+            String stringTemp = "";
+            for (int j = 0 ; j < str2.length() ; j++){
+                stringTemp = stringTemp + str1.charAt(j + i);
+            }
+            //converting the string to an array
+            char[] arr1 = stringToArray(stringTemp);
+            char[] arr2 = stringToArray(str2);
+
+            //checks if the arrays are similar
+            if (equalsArray(arr1, arr2)){
+                return true;
+            }
+        }
         return false;
     }
 }
